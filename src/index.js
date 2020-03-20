2802b6440029fbf0f03227423fb90de352973f2e
mbjelac <marko.bjelac@softwaresauna.com>
2020-03-20 10:19:01 +0100

    Implemented source URL input & query param

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import {getData} from "./data";
 import axios from "axios";
 
-
-axios.get("https://raw.githubusercontent.com/softwaresauna/story-tag-results-test/master/story-tags.txt")
-    .then(response => {
-        const storyTagsText = response.data;
-
-        const chartData = getData(storyTagsText);
-
-        // Create a new line chart object where as first parameter we pass in a selector
-        // that is resolving to our chart container element. The Second parameter
-        // is the actual data object.
-        new Chartist.Line('.ct-chart', chartData);
-    });
+// "https://raw.githubusercontent.com/softwaresauna/story-tag-results-test/master/story-tags.txt"
 
 
+function loadData(sourceUrl) {
+
+    if (!sourceUrl){
+        console.log("Loading data aborted: No URL in query param 'sourceUrl'.");
+        return;
+    }
+
+    axios.get(sourceUrl)
+        .then(response => {
+            const storyTagsText = response.data;
+
+            const chartData = getData(storyTagsText);
+
+            // Create a new line chart object where as first parameter we pass in a selector
+            // that is resolving to our chart container element. The Second parameter
+            // is the actual data object.
+            new Chartist.Line('.ct-chart', chartData);
+        });
+}
+
+const sourceUrl = new URLSearchParams(window.location.search).get("sourceUrl");
+
+loadData(sourceUrl);
