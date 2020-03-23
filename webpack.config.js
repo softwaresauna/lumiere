8e3510006ddaf7732dc7dc20d45fda31705e2604
mbjelac <marko.bjelac@softwaresauna.com>
2020-03-23 09:12:26 +0100

    Added typescript support

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,24 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    devtool: 'inline-source-map',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'public/dist'),
     },
     devServer: {
         contentBase: './public',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
     },
 };
